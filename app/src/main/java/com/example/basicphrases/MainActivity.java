f71bf06f4cd98f59b82cdced46dea7e1784b40ab
Tashi Jain <tashjain88@gmail.com>
2020-06-18 19:52:45 -0700

    plays phrase

--- app/src/main/java/com/example/basicphrases/MainActivity.java
+++ app/src/main/java/com/example/basicphrases/MainActivity.java
@@ -2,9 +2,27 @@ package com.example.basicphrases;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    public void playPhrase(View view) {
+
+        Button buttonPressed = (Button) view;
+
+        //Log.i("Button pressed", buttonPressed.getTag().toString());
+
+        //play sound file as variable
+        MediaPlayer mediaPlayer = MediaPlayer.create(this,
+                getResources().getIdentifier(buttonPressed.getTag().toString(),
+                "raw", getPackageName()));
+
+        mediaPlayer.start();
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
